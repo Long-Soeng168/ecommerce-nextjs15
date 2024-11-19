c22c14231c0375f75bb4243ea1134b814b830732
Long-Soeng168 <generalpurpose167@gmail.com>
2024-11-19 14:36:13 +0700

    update stepper

--- src/components/my-stepper.jsx
+++ src/components/my-stepper.jsx
@@ -1,5 +1,5 @@
 // components/MyStepper.js
-import { CheckCircle } from "lucide-react";
+import { CheckCircle, PackageCheck, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -8,24 +8,24 @@ const MyStepper = ({ currentStep }) => {
     <div className="flex items-center justify-center p-2 py-8 space-x-8">
       {/* Cart Step */}
       <Link href='/cart' className="flex items-center space-x-2">
-        <CheckCircle className={currentStep >= 1 ? "text-blue-600" : "text-gray-400"} />
-        <span className={currentStep >= 1 ? "text-blue-600" : "text-gray-400"}>Cart</span>
+        <ShoppingCart className={currentStep >= 1 ? "text-primary" : "text-gray-400"} />
+        <span className={currentStep >= 1 ? "text-primary" : "text-gray-400"}>Cart</span>
       </Link>
 
-      <div className="flex-1 border-t border-gray-200"></div>
+      <div className={currentStep === 2 ? "flex-1 border-t border-primary border-2" : "flex-1 border-t border-2 border-gray-400"}></div>
 
       {/* Checkout Step */}
       <Link href='/cart/checkout' className="flex items-center space-x-2">
-        <CheckCircle className={currentStep === 2 ? "text-blue-600" : "text-gray-400"} />
-        <span className={currentStep === 2 ? "text-blue-600" : "text-gray-400"}>Checkout</span>
+        <PackageCheck className={currentStep === 2 ? "text-primary" : "text-gray-400"} />
+        <span className={currentStep === 2 ? "text-primary" : "text-gray-400"}>Checkout</span>
       </Link>
 
-      <div className="flex-1 border-t border-gray-200"></div>
+      <div className="flex-1 border-2 border-t border-gray-400"></div>
 
       {/* Order Summary Step */}
       <div className="flex items-center space-x-2">
-        <CheckCircle className={currentStep === 3 ? "text-blue-600" : "text-gray-400"} />
-        <span className={currentStep === 3 ? "text-blue-600" : "text-gray-400"}>Success</span>
+        <CheckCircle className={currentStep === 3 ? "text-primary" : "text-gray-400"} />
+        <span className={currentStep === 3 ? "text-primary" : "text-gray-400"}>Success</span>
       </div>
     </div>
   );
