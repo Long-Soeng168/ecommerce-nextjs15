a054b5594f02589dbaa40f2ef92eca99b7f5076d
Long-Soeng168 <generalpurpose167@gmail.com>
2024-11-27 16:38:50 +0700

    update

--- src/components/my-category-list.jsx
+++ src/components/my-category-list.jsx
@@ -4,12 +4,16 @@ import MyCategoryCard from "./ui/my-category-card";
 import { getCategories } from "@/services/categories-services";
 
 const MyCategoryList = async () => {
-  const categories = await getCategories({ limit: 4, orderBy: 'books_count', orderDir: 'desc' });
+  const categories = await getCategories({
+    limit: 4,
+    orderBy: "books_count",
+    orderDir: "desc",
+  });
   return (
     <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-4">
       {categories?.map((category) => (
         <MyCategoryCard category={category} key={category.id} />
-      ))}
+      ))} 
     </div>
   );
 };
--- src/components/ui/my-category-card.jsx
+++ src/components/ui/my-category-card.jsx
@@ -6,8 +6,9 @@ const MyCategoryCard = ({ category }) => {
   return (
     <>
       <Link href="/products">
-        <div className="flex items-center h-full p-4 transition-shadow duration-300 bg-white border border-gray-200 rounded-lg shadow cursor-pointer hover:shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-          <div className="p-1 mr-4 text-4xl bg-white rounded-md dark:bg-gray-200">
+        <div className="relative flex items-center h-full p-4 overflow-hidden transition-shadow duration-300 bg-white border border-gray-200 rounded-lg cursor-pointer group hover:shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <span className="absolute bottom-0 left-0 w-full h-48 mb-0 ml-0 transition-all duration-300 ease-out -translate-x-full translate-y-full bg-indigo-500 rounded group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <div className="z-50 p-1 mr-4 text-4xl bg-white rounded-md dark:bg-gray-200">
             <Image
               className={`aspect-square object-contain`}
               width={40}
@@ -16,12 +17,12 @@ const MyCategoryCard = ({ category }) => {
               alt=""
             />
           </div>
-          <div>
-            <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100">
+          <div className="z-50">
+            <h3 className="text-lg font-semibold text-gray-800 group-hover:text-white dark:text-gray-100">
               {category.name}
             </h3>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
-              {category.books_count ? category.books_count + '+ Books' : ''}
+            <p className="text-sm text-gray-600 group-hover:text-white dark:text-gray-400">
+              {category.books_count ? category.books_count + "+ Books" : ""}
             </p>
           </div>
         </div>
