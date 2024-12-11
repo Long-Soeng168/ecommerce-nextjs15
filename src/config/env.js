537ebf1d7012f2827864746d85eaee2eab9efcf8
Long-Soeng168 <generalpurpose167@gmail.com>
2024-12-11 14:04:22 +0700

    Stop tracking env.js

--- src/config/env.js
+++
@@ -1,4 +0,0 @@
-const IMAGE_BOOK_URL = 'https://scholar.brolong.pro/assets/images/isbn/';
-const IMAGE_CATE_URL = 'https://scholar.brolong.pro/assets/images/categories/';
-
-export { IMAGE_BOOK_URL, IMAGE_CATE_URL };
