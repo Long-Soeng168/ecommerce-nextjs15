3671e10a9aeeba989314a69f1359c16ef16f93b8
Long-Soeng168 <generalpurpose167@gmail.com>
2024-12-11 13:21:57 +0700

    update

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,5 @@
 const IMAGE_BOOK_URL = 'https://scholar.brolong.pro/assets/images/isbn/';
 const IMAGE_CATE_URL = 'https://scholar.brolong.pro/assets/images/categories/';
+const IMAGE_LINK_URL = 'https://scholar.brolong.pro/assets/images/links/';
 
 export { IMAGE_BOOK_URL, IMAGE_CATE_URL };
