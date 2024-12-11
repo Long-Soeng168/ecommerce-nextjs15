78e2848ba1be6d0ae849b4496644b506450bc375
Long-Soeng168 <generalpurpose167@gmail.com>
2024-12-11 10:40:13 +0700

    update

--- src/app/[locale]/(client)/cart/checkout/page.jsx
+++ src/app/[locale]/(client)/cart/checkout/page.jsx
@@ -75,7 +75,7 @@ export default function Component() {
     setLoading(true);
 
     try {
-      const response = await fetch("https://scholar.brolong.pro/api/orders", {
+      const response = await fetch(process.env.BASE_API_URL + "/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
