ebccd187be02a22f2c9c8ac0c8dc1aa049657061
Long-Soeng168 <generalpurpose167@gmail.com>
2024-12-13 14:16:05 +0700

    update

--- src/app/[locale]/pdf-viewer/page.jsx
+++ src/app/[locale]/pdf-viewer/page.jsx
@@ -1,16 +1,28 @@
 "use client";
 
-import PDFViewer from '@/components/pdf-viewer'
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import PDFViewer from "@/components/pdf-viewer"; // Import your PDFViewer component
 
 const Page = () => {
+  const [isClient, setIsClient] = useState(false); // State to check if we are in the client browser
+  // Use useEffect to update the state when the component mounts on the client side
+  useEffect(() => {
+    setIsClient(true); // Set to true once the component mounts
+  }, []);
+
+  if (!isClient) {
+    // If it's not the client side, return null (or a loading indicator)
+    return null;
+  }
+  console.log("running");
+
   return (
     <div>
-    <PDFViewer/>
-    <PDFViewer pdfUrl='https://admin.thnal.org/sample.pdf'/>
+      {/* Render the PDFViewer only on the client */}
+      <PDFViewer />
+      <PDFViewer pdfUrl="https://admin.thnal.org/sample.pdf" />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
