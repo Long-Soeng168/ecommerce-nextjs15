48aa802b768a2a732195836ee621bafc61df5697
Long-Soeng168 <generalpurpose167@gmail.com>
2024-12-17 18:56:56 +0700

    change production login api

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -23,7 +23,7 @@ export function LoginForm({ className, ...props }) {
   const router = useRouter();
 
   useEffect(() => {
-    setBaseUrl(BASE_API_URL || "http://127.0.0.1:8000/api");
+    setBaseUrl(BASE_API_URL || "https://admin.thnal.org/api");
     const token = localStorage.getItem("token");
     if (token) {
       router.push("/admin/pos");
